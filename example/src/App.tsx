db70a952fa1a2c18cd2e8c3b1f063ff5d5a9e435
Karim Saad <Karim.Saad@macbooks-MacBook-Pro-2.local>
2022-11-24 17:35:57 +0200

    change app.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   const test = () => {
     const path='/data/user/0/com.mazadatimagepicker6example/cache/1669118307971.png'
-    openCamera('Scan Front Side',4, 7).then((value) => {
+    openCamera('Scan Front Side',1, 1).then((value) => {
       setResult(value)
       console.log(result)
     });
